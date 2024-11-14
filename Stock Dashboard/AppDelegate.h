19433999ab6ef48ad44024b925024e6d1f6c8c36
sunami09 <sunamidasgupta@gmail.com>
2024-11-14 08:41:58 -0800

    Initial Commit

---
+++ Stock Dashboard/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Stock Dashboard
+//
+//  Created by Sunami Dasgupta on 11/14/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Stock Dashboard/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Stock Dashboard
+//
+//  Created by Sunami Dasgupta on 11/14/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
